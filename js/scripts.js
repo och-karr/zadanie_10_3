87be79f0c329cb96ca64995f2ec9979289cf4c10
karolinneczka <okaa93@gmail.com>
2018-10-17 17:17:41 +0200

    Add next photo button

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,23 @@
 $(function(){
     var carouselList = $('#carousel ul');
     setInterval(function changeSlide(){
-        carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-    }, 3000);
+        carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide); //ostatni parametr animacji - funkcja, która zadziała po zakończeniu animacji
+    }, 4000);
+
+    
+
     function moveFirstSlide(){
         var firstItem = carouselList.find('li:first');
         var lastItem = carouselList.find('li:last');
-        $(lastItem).after(firstItem);
-        $(carouselList).css({marginLeft:0});
+        $(lastItem).after(firstItem); //dodaje za ostatnim elementem pierwszy element (metoda after)
+        $(carouselList).css({marginLeft:0}); //resetuje lewy margines z powrotem na wartość 0 (metoda css)
     };
+
+    $(function nextPhoto(){
+        var nextButton = $('#next');
+        nextButton.click(function(){
+            carouselList.animate({'marginLeft':0}, 0, moveFirstSlide);
+        });
+    });
 });
 
